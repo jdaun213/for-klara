e1acebd59ff003b6fffbd240c71382f150b276e7
jdaun213 <jdaun10@naver.com>
2025-01-09 19:18:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 document.getElementById("show-love").addEventListener("click", function () {
   const loveMessage = document.getElementById("love-message");
   loveMessage.style.display = "block"; // 메시지 표시
+
+  const music = document.getElementById("background-music");
+  music.play(); // 배경 음악 재생
 });
