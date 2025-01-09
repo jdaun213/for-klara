9bd61390a8821af7d7d187ffa8d94a2385fcc5f2
jdaun213 <jdaun10@naver.com>
2025-01-09 19:15:27 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("show-love").addEventListener("click", function () {
+  const loveMessage = document.getElementById("love-message");
+  loveMessage.style.display = "block"; // 메시지 표시
+});
